817f1569f7d70fa9df02aec9e96f8ca7e0c3915e
jundysa <156988752+jundysa@users.noreply.github.com>
2024-01-18 12:24:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+pindah = 1;
+function new (){
+    if(pindah == 1){
+        Btn.style.top=400;
+        Btn.style.top=300;
+        pindah=2;
+    }
+    else if( pindah == 2){
+        Btn.style.top=400;
+        Btn,style.left=50);
+        pindah=3;
+    }
+    else if( pindah == 3){
+        Btn.style.top=370;
+        Btn,style.left=166);
+        pindah=1;
+    }
+}
